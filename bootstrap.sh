cf7d9a955e9397c978f037e52ed85c9fb2648850
codrcodz <cody.l.cochran@gmail.com>
2019-05-20 11:34:46 -0500

    Add support for Ansible 2.8
    Prior to this change, this playbook used Ansible 2.7; some slight
    refactors to the python-packages tasks file was required to get it work
    with Ansible 2.8 properly.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ if [[ "${EUID}" != "0" ]] || [[ "${#SUDO_USER}" != "0" ]]; then
   exit 1;
 fi
 
-apt-get update && apt-get install -y python3-pip && pip3 install ansible==2.7;
+apt-get update && apt-get install -y python3-pip && pip3 install ansible==2.8;
 
